1e41058261f0f031e303f07e3d9899ff1cd3d5d5
hallucigenia <air.bylive@gmail.com>
2018-10-18 19:23:56 +0800

    add extensions.py

--- crypticlog/__init__.py
+++ crypticlog/__init__.py
@@ -13,13 +13,55 @@ def create_app(config_name=None):
     app = Flask('crypticlog')
     app.config.from_object(config[config_name])
 
+    register_logging(app)
+    register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
+    register_errors(app)
+    register_shell_context(app)
+    register_template_context(app)
+    return app
+
+def register_logging(app):
+    pass # not finished
+
+def register_extensions(app):
+    bootstrap.init_app(app)
+    db.init_app(app)
+    ckeditor.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+
+def register_blueprints(app):
     app.register_blueprint(blog)
     app.register_blueprint(admin, url_prefix='/admin')
     app.register_blueprint(auth, url_prefix='/auth')
-    return app
+
+def register_shell_context(app):
+    @app.shell_context_processor
+    def make_shell_context():
+        return dict(db=db)
+
+def register_template_context(app):
+    pass
+
+def register_errors(app):
+    @app.errorhandler(400)
+    def bad_request(e):
+        return render_template('errors/400.html'), 400
+
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return render_template('errors/404.html'), 400
+
+    @app.errorhandler(500)
+    def internal_server_error(e):
+        return render_template('errors/500.html'), 500
+
+    @app.errorhandler(CSRFError)
+    def handle_csrf_error(e):
+        return render_template('errors/400.html', description=e.description), 400
 
 
+def register_commands(app):
 
-def register_blueprints(app):
-    app.register_blueprint(auth_bp)
-    app.register_blueprint(auth_bp, url_prefix='/auth')
---
+++ crypticlog/extensions.py
@@ -0,0 +1,11 @@
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import sqlalchemy
+from flask_mail import Mail
+from flask_ckeditor import CKEditor
+from flask_moment import Moment
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+moment = Moment()
+ckeditor = CKEditor()
+mail = Mail()
