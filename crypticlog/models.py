217585e84b40f85b30ab23ac5d30f243bee36aee
hallucigenia <air.bylive@gmail.com>
2018-10-19 16:20:03 +0800

    modified models.py

--- crypticlog/models.py
+++ crypticlog/models.py
@@ -13,8 +13,35 @@ class Admin(db.Model):
     name = db.Column(db.String(30))
     about = db.Column(db.Text)
 
+class Category(db.Model):
+
+    posts = db.relationship('Post', back_populates='category')
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(60))
     body = db.Column(db.Text)
     timestamp = db.Column(db.DataTime, default=datetime.utcnow)
+
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id')
+
+    category = db.relationship('Category', back_populates='posts')
+    comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(30))
+    email = db.Column(db.String(254))
+    site = db.Column(db.String(255))
+    body = db.Column(db.Text)
+    from_admin = db.Column(db.Boolean, default=False)
+    reviewed = db.Column(db.Boolean, default=False)
+    timestamp = db.Column(db.DateTime, default=datatime.utcnow, index=True)
+
+    replied_id = db.Column(db.Integer, db.ForeignKey('comment.id'))
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+
+    post = db.relationship('Post', back_populates='comments')
+    replied = db.relationship('Comment', back_populates="replies", remote_side=[id])
+    replies = dv.relationship('Comment', back_populates='replied', cascade='all')
