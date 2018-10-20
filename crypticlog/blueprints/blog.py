8a01b1b10cabbdc59ae8a8606664080b1bbc85f0
hallucigenia <air.bylive@gmail.com>
2018-10-20 20:07:27 +0800

    modified blog theme

--- crypticlog/blueprints/blog.py
+++ crypticlog/blueprints/blog.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = 'fansly'
 
-from flask import render_template, Blueprint, request
+from flask import render_template, Blueprint, request, abort, make_response
 
 from crypticlog.models import Comment
 from crypticlog.forms import AdminCommentForm, CommentForm
 from crypticlog.emails import send_new_comment_email, send_new_reply_email
+from crypticlog.utils import redirect_back
 
 blog_bp = Blueprint('blog', __name__)
 
@@ -77,3 +78,12 @@ def show_post(post_id):
 def reply_comment(comment_id):
     comment = Comment.query.get_or_404(comment_id)
     return redirect(url_for('.show_post', post_id=comment.post_id, reply=comment_id, author=comment.author) + '#comment-form')
+
+@blog_bp.route('/change-theme/<theme_name>')
+def change_theme(theme_name):
+    if theme_name not in current_app.config['CRYPTICLOG_THEMES'].keys():
+        abort(404)
+
+    response = make_response(redirect_back())
+    response.set_cookie('theme', theme_name, max_age=30*24*60*60)
+    return response
