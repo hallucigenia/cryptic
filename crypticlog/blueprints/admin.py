53cc04f8518709526fd08ecc3300b3e8227b8a10
hallucigenia <air.bylive@gmail.com>
2018-10-22 19:48:43 +0800

    add comment manage

--- crypticlog/blueprints/admin.py
+++ crypticlog/blueprints/admin.py
@@ -68,6 +68,23 @@ def set_comment(post_id):
     db.session.commit()
     return redirect(url_for('.show_post', post_id=post_id))
 
+@admin_bp.route('/comment/manage')
+@login_required
+def manage_comment():
+    filter_rule = request.args.get('filter', 'all') # 'all', 'unreviewed', 'admin'
+    page = request.args.get('page', 1, type=int)
+    per_page = current_app.config['CRYPTICLOG_COMMENT_PER_PAGE']
+    if filter_rule == 'unread':
+        filtered_comments = Comment.query.filter_by(reviewed=False)
+    elif filter_rule == 'admin':
+        filtered_comments = Comment.query.filter_by(from_admin=True)
+    else:
+        filtered_comments = Comment.query
+
+    pagination = filtered_comments.order_by(Comment.timestamp.desc()).paginate(page, per_page=per_page)
+    comments = pagination.items
+    return render_template('admin/manage_comment.html', comments=comments, pagination=pagination)
+
 @admin_bp.route('/comment/<int:comment_id>/approve')
 @login_required
 def approve_comment(comment_id):
