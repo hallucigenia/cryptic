b275bb1f306c64ea23ba6c9a4a4f16f5dfa7fb31
hallucigenia <air.bylive@gmail.com>
2018-10-22 23:22:30 +0800

    finished extensions

--- crypticlog/extensions.py
+++ crypticlog/extensions.py
@@ -6,12 +6,20 @@ from flask_sqlalchemy import sqlalchemy
 from flask_mail import Mail
 from flask_ckeditor import CKEditor
 from flask_moment import Moment
+from flask_login import LoginManager
+from flask_wtf import CSRFProtect
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_migrate import Migrate
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 moment = Moment()
 ckeditor = CKEditor()
 mail = Mail()
+login_manager = LoginManager()
+csrf = CSRFProject()
+toolbar = DebugToolbarExtension()
+migrate = Migrate()
 
 
 @login_manager.user_loader
@@ -19,3 +27,7 @@ def load_user(user_id):
     from Crypticlog.models import Admin
     user = Admin.query.get(int(user_id))
     return user
+
+login_manager.login_view = 'auth.login'
+# login_manager.login_message = "Your custom message"
+login_manager.login_message_category = 'warning'
