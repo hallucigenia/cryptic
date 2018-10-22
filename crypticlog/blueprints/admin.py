d8860e6cd330d9c65a4d0c45254be2a404d0461c
hallucigenia <air.bylive@gmail.com>
2018-10-22 15:03:45 +0800

    modified create post

--- crypticlog/blueprints/admin.py
+++ crypticlog/blueprints/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'fansly'
 
+from crypticlog.forms import PostForm
+from crypticlog.models import Post, Category
+
 @admin_bp.route('/post/manage')
 @login_required
 def manage_post():
@@ -9,3 +12,18 @@ def manage_post():
         page, per_page=current_app.config['CRYPTICLOG_MANAGE_POST_PER_PAGE'])
     posts = pagination.items
     return render_template('admin/manage_post.html', pagination=pagination, posts=posts)
+
+@admin_bp.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        body = form.body.data
+        category = Category.query.get(form.category.data)
+        post = Post(title=title, body=body, category=category)
+        db.session.add(post)
+        db.session.commit()
+        flash('Post created.', 'success')
+        return redirect(url_for('.show_post', post_id=post.id))
+    return render_template('admin/new_post.html', form=form)
