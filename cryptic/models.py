31da9633475507ceab24f7d342cbb8cfdcab82c3
hallucigenia <air.bylive@gmail.com>
2018-11-22 15:08:07 +0800

    finished show post

--- cryptic/models.py
+++ cryptic/models.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = 'fansly'
 
-import bleach
 from datetime import datetime
-from markdown import markdown
 
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -54,18 +52,6 @@ class Post(db.Model):
 
     category = db.relationship('Category', back_populates='posts')
     comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')
-
-    @staticmethod
-    def on_changed_body(target, value, oldvalue, initiator):
-        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
-                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
-                        'h1', 'h2', 'h3', 'p']
-        target.body_html = bleach.linkify(bleach.clean(
-            markdown(value, output_format='html'),
-            tags=allowed_tags, strip=True))
-
-
-db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 
 class Comment(db.Model):
